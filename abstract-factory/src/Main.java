caac27932169096d82041bd764a3fa075b19fa33
roccodelprete <122788299+roccodelprete@users.noreply.github.com>
2023-11-13 16:52:45 +0100

    fix products types

--- abstract-factory/src/Main.java
+++ abstract-factory/src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         ConcreteFactory1 factory1 = new ConcreteFactory1();
         ConcreteFactory2 factory2 = new ConcreteFactory2();
 
-        ProductA1 productA1 = (ProductA1) factory1.createProductA();
-        ProductA2 productA2 = (ProductA2) factory2.createProductA();
+        AbstractProductA productA1 = factory1.createProductA();
+        AbstractProductA productA2 = (ProductA2) factory2.createProductA();
 
-        ProductB1 productB1 = (ProductB1) factory1.createProductB();
-        ProductB2 productB2 = (ProductB2) factory2.createProductB();
+        AbstractProductB productB1 = factory1.createProductB();
+        AbstractProductB productB2 = factory2.createProductB();
 
         productA1.setProductName("Audi");
         productA2.setProductName("Mercedes");
